94dfd119b6d2fad286a58b2160b872cb254825aa
Michal-d <black.solo.fan@gmail.com>
2025-09-06 18:15:21 +0200

    Revert Tailwind CSS build process due to local env issues

--- service-worker.js
+++ service-worker.js
@@ -2,3 +2,7 @@
 self.addEventListener('install', (event) => {
     console.log('Service worker installing...');
 });
+
+self.addEventListener('fetch', (event) => {
+    // Na razie nie robimy nic z żądaniami sieciowymi
+});
